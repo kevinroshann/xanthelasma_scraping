d2d8f9e470632906bd2f8876b2c37b4f5b813f9c
Kevin Roshan N <kevinroshann@gmail.com>
2024-03-26 02:20:10 -0400

    yahoo

---
+++ scrapeyahoo.py
@@ -0,0 +1,119 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import requests
+import io
+from PIL import Image
+import time
+
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+
+# Assuming chromedriver.exe is in the same directory or your PATH
+options = Options()
+PATH = r"C:\Users\ekn\Downloads\edgedriver_win64\msedgedriver.exe"
+service = Service(executable_path=PATH)
+
+wd = webdriver.Chrome(options=options)
+
+
+def get_images_from_google(wd, delay, max_images):
+    def scroll_down(wd):
+        wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(delay)
+
+    url = "https://images.search.yahoo.com/search/images;_ylt=AwrNPHpTKQJmAwMIMSCJzbkF;_ylu=c2VjA3NlYXJjaARzbGsDYnV0dG9u;_ylc=X1MDOTYwNjI4NTcEX3IDMgRmcgMEZnIyA3A6cyx2OmksbTpzYi10b3AEZ3ByaWQDBG5fcnNsdAMwBG5fc3VnZwMwBG9yaWdpbgNpbWFnZXMuc2VhcmNoLnlhaG9vLmNvbQRwb3MDMARwcXN0cgMEcHFzdHJsAzAEcXN0cmwDMTEEcXVlcnkDeGFudGhlbGFzbWEEdF9zdG1wAzE3MTE0MzIwODg-?p=xanthelasma&fr=&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&ei=UTF-8&x=wrt"
+    wd.get(url)
+
+    downloaded_urls_file = "downloaded_urls.txt"  # File to store downloaded URLs
+    image_urls = set()
+
+    # Check if downloaded URLs file exists and load them
+    if os.path.exists(downloaded_urls_file):
+        with open(downloaded_urls_file, "r") as f:
+            for line in f:
+                image_urls.add(line.strip())
+
+    last_height = wd.execute_script("return document.body.scrollHeight")
+
+    while len(image_urls) < max_images:
+        scroll_down(wd)
+
+        # Check if new images loaded after scrolling
+        new_height = wd.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            try:
+                # Click "Load More" button using XPath (attempt 1)
+                load_more_button = wd.find_element(By.XPATH, "//button[text()='Show more images']")
+                load_more_button.click()
+                time.sleep(delay)
+            except:
+                try:
+                    # Click "Load More" button using partial link text (attempt 2)
+                    more_results_link = wd.find_element(By.PARTIAL_LINK_TEXT, "Show more images")
+                    more_results_link.click()
+                    time.sleep(delay)
+                except:
+                    pass  # No "Load More" button found, continue scraping
+
+        last_height = new_height
+
+         
+        
+        image = wd.find_element(By.CSS_SELECTOR, "img[src*='http']")
+        if image.get_attribute('src') in image_urls:
+            continue
+
+        if image.get_attribute('src') and 'http' in image.get_attribute('src'):
+            image_urls.add(image.get_attribute('src'))
+            print(f"Found {len(image_urls)}")
+            print(image.get_attribute('src'))
+
+            # Download the image and update downloaded URLs file
+            download_image("imgsyahoo/", image.get_attribute('src'), str(len(image_urls)) + ".jpg")
+            with open(downloaded_urls_file, "a") as f:
+                f.write(image.get_attribute('src') + "\n")
+            return image_urls
+            # print(thumbnails)
+    #     for img in thumbnails:
+
+    #         images = wd.find_elements(By.CSS_SELECTOR, "img[src*='http']")
+    #         print(images)
+    #         for image in images:
+    #             if image.get_attribute('src') in image_urls:
+    #                 continue
+
+    #             if image.get_attribute('src') and 'http' in image.get_attribute('src'):
+    #                 image_urls.add(image.get_attribute('src'))
+    #                 print(f"Found {len(image_urls)}")
+    #                 print(image.get_attribute('src'))
+
+    #                 # Download the image and update downloaded URLs file
+    #                 download_image("imgsedge/", image.get_attribute('src'), str(len(image_urls)) + ".jpg")
+    #                 with open(downloaded_urls_file, "a") as f:
+    #                     f.write(image.get_attribute('src') + "\n")
+
+    # return image_urls
+
+
+def download_image(download_path, url, file_name):
+    try:
+        image_content = requests.get(url).content
+        image_file = io.BytesIO(image_content)
+        image = Image.open(image_file)
+        file_path = download_path + file_name
+
+        with open(file_path, "wb") as f:
+            image.save(f, "JPEG")
+
+        print("Success")
+    except Exception as e:
+        print('FAILED -', e)
+
+urls = get_images_from_google(wd, 1, 3000)  # Change max_images to your desired limit
+
+for i, url in enumerate(urls):
+    download_image("imgsyahoo/", url, str(i) + ".jpg")
+
+wd.quit()
